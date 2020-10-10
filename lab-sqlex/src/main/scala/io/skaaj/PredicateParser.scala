7af9846465a2ebddb492b7f83faafd5b676ce2fe
Benjamin Denom <ben.denom@gmail.com>
2020-10-10 15:54:45 +0200

    clean up predicate parser

--- lab-sqlex/src/main/scala/io/skaaj/PredicateParser.scala
+++ lab-sqlex/src/main/scala/io/skaaj/PredicateParser.scala
@@ -1,25 +1,39 @@
 package io.skaaj
 
-import fastparse._, fastparse.NoWhitespace._
+import fastparse.NoWhitespace._
+import fastparse._
 
 object PredicateParser {
   def apply[_: P]: P[Expression] = P(orExpression ~ End)
 
-  private def ws[_: P]: P[Unit] = P(CharIn(" \t").rep(1))
-  def identifier[_: P]: P[Expression] = P(CharIn("a-z0-9").rep(1)).!.map(Identifier)
-  def parens[_: P]: P[Expression] = P("(" ~ orExpression ~ ")")
-  def boolValue[_: P]: P[Expression] = P("true" | "false").!.map(x => Constant(x.toBoolean))
-  def handSide[_: P]: P[Expression] = P(notExpression | boolValue | identifier | parens)
-  def notExpression[_: P]: P[Expression] = P("not" ~ ws ~ handSide).map(Not)
-  def andExpression[_: P]: P[Expression] = P(handSide ~ (ws ~ "and" ~ ws ~ handSide).rep).map {
-    case (x, xs) => xs.foldLeft(x) {
-      case (left, right) => And(left, right)
-    }
-  }
+  private def ws[_: P]: P[Unit] =
+    P(CharIn(" \t").rep(1))
 
-  def orExpression[_: P]: P[Expression] = P(andExpression ~ (ws ~ "or" ~ ws ~ andExpression).rep).map {
+  private def identifier[_: P]: P[Expression] =
+    P(CharIn("a-z0-9").rep(1)).!.map(Identifier)
+
+  private def parens[_: P]: P[Expression] =
+    P("(" ~ orExpression ~ ")")
+
+  private def constant[_: P]: P[Expression] =
+    P("true" | "false").!.map(x => Constant(x.toBoolean))
+
+  private def handSide[_: P]: P[Expression] =
+    P(notExpression | constant | identifier | parens)
+
+  private def notExpression[_: P]: P[Expression] =
+    P("not" ~ ws ~ handSide).map(Not)
+
+  private def andExpression[_: P]: P[Expression] =
+    P(handSide ~ (ws ~ IgnoreCase("and").! ~ ws ~ handSide).rep).map(buildBinaryExpression)
+
+  private def orExpression[_: P]: P[Expression] =
+    P(andExpression ~ (ws ~ IgnoreCase("or").! ~ ws ~ andExpression).rep).map(buildBinaryExpression)
+
+  private def buildBinaryExpression(captured: (Expression, Seq[(String, Expression)])): Expression = captured match {
     case (x, xs) => xs.foldLeft(x) {
-      case (left, right) => Or(left, right)
+      case (left, (operator, right)) if operator.equalsIgnoreCase("or") => Or(left, right)
+      case (left, (operator, right)) if operator.equalsIgnoreCase("and") => And(left, right)
     }
   }
 }
