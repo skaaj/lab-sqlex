8708ebc6212a50b8d80ac01639501e9b5e57a7b4
Benjamin Denom <ben.denom@gmail.com>
2020-10-10 19:10:12 +0200

    add result column types (select parser)

--- lab-sqlex/src/main/scala/io/skaaj/Main.scala
+++ lab-sqlex/src/main/scala/io/skaaj/Main.scala
@@ -4,28 +4,47 @@ import fastparse._, NoWhitespace._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    case class Select(items: Seq[SelectItem], distinct: Boolean) extends Expression {
-      override def toString: String = s"Select(distinct: $distinct, cols: [${items.mkString(", ")}])"
-    }
-    case class SelectItem(value: String) extends Expression {
-      override def toString: String = value
+
+    sealed trait ResultColumn {
+      val alias: Option[String]
     }
 
-    def ws[_: P]: P[Unit] = P(CharIn(" \t").rep(1))
-    def selectItemPrefix[_: P]: P[String] = P(CharIn("a-z").rep(1).! ~ ".")
-    def selectItemSuffix[_: P]: P[String] = P((IgnoreCase("as") ~ ws).? ~ CharIn("a-z").rep(1).!)
-    def selectItem[_: P]: P[String] = P(selectItemPrefix.? ~ (CharIn("a-z").rep(1) | "*").! ~ ( ws ~ selectItemSuffix).?).map {
-      case (objectName, colName, colAlias) =>
-        objectName.map(x => s"$x.").getOrElse("") + colName + colAlias.map(x => s" AS $x").getOrElse("")
-    }
-    def selectToken[_: P]: P[Unit] = P(IgnoreCase("select"))
-    def duplicateToken[_: P]: P[String] = P(StringInIgnoreCase("all","distinct")).!.map(_.toLowerCase)
-    def select[_: P]: P[Expression] = P(selectToken ~ (ws ~ duplicateToken).? ~ ws ~ (selectItem ~ ws.rep ~ "," ~ ws.rep).rep ~ selectItem ~ End).map {
-      case (duplicateOption, initCols, lastCol) =>
-        Select((initCols :+ lastCol).map(SelectItem), duplicateOption.contains("distinct"))
+    case class ColumnFromRef(sourceObject: Option[String], sourceColumn: String, alias: Option[String] = None)
+      extends ResultColumn
+
+    case class ColumnFromExpr(expr: String, alias: Option[String] = None)
+      extends ResultColumn
+
+    case class Select(distinct: Boolean,
+                      columns: Seq[ResultColumn]) extends Expression {
+      override def toString: String = s"Select(distinct: $distinct, columns: [${columns.mkString(", ")}])"
     }
 
-    val input = "SELECT foo ass, bar"
+    def ` `[_: P]: P[Unit] = P(CharIn(" \t\n").rep(1))
+    def _select[_: P]: P[Unit] = P(IgnoreCase("select"))
+    def _duplicateMode[_: P]: P[String] = P(StringInIgnoreCase("all","distinct")).!.map(_.toLowerCase)
+    def _as[_: P]: P[Unit] = P(IgnoreCase("as"))
+
+
+    def rcSourceObject[_: P]: P[String] = P(CharIn("a-z").rep(1).! ~ ".")
+    def rcSourceColumn[_: P]: P[String] = P(CharIn("a-z").rep(1) | "*").!
+    def rcAlias[_: P]: P[String] = P((_as ~ ` `).? ~ CharIn("a-zA-Z").rep(1).!)
+    def rcSeparator[_: P]: P[Unit] = P(` `.? ~ "," ~ ` `.?)
+    def rc[_: P]: P[ResultColumn] = P(rcSourceObject.? ~ rcSourceColumn ~ (` ` ~ rcAlias).?).map {
+      case (sourceObject, sourceColumn, alias) => ColumnFromRef(sourceObject, sourceColumn, alias)
+    }
+    def resultColumns[_: P]: P[(Seq[ResultColumn], ResultColumn)] = P((rc ~ rcSeparator).rep ~ rc)
+    def duplicateMode[_: P]: P[String] = P(_duplicateMode ~ ` `)
+    def select[_: P]: P[Expression] = P(` ` ~ _select ~ ` ` ~ duplicateMode.? ~ resultColumns ~ End).map {
+      case (duplicateOption, (initCols, lastCol)) =>
+        Select(duplicateOption.contains("distinct"), (initCols :+ lastCol))
+    }
+
+    val input =
+      s"""
+         |Select foo as foul,
+         |bar,
+         |tabletwo.baz as bazile""".stripMargin
     val result = parse(input, select(_))
     result match {
       case Parsed.Success(result, index) =>
